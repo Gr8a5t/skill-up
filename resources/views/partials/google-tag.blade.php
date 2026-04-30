3d6b22613a507bf4f266e3179de1b4b1fb8b2971
Great Onweazu <greatonweazu48@gmail.com>
2026-04-30 14:59:13 +0100

    initail commit

--- resources/views/partials/google-tag.blade.php
+++ resources/views/partials/google-tag.blade.php
@@ -1,9 +1,9 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-3GE48NCHC5"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JESTY1G8NS"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-3GE48NCHC5');
+  gtag('config', 'G-JESTY1G8NS');
 </script>
